8b8d17a155f24fb140a052dd45886ea185de6fb6
Takashi Masuyama <mamewotoko@gmail.com>
2012-09-01 23:29:57 +0900

    use ANDROID_HOME environment variable to start adb
    move coverage report into bin directory

--- test/podtest/run.sh
+++ test/podtest/run.sh
@@ -1,16 +1,18 @@
 #! /bin/sh
 COVERAGE_FILE=/data/data/com.mamewo.podplayer0/coverage.ec
 CLASS_LIST="PodplayerExpActivityTest PodplayerActivityTest"
+ADB=$ANDROID_HOME/platform-tools/adb
 
 #adb shell rm -f $COVERAGE_FILE
 for class in $CLASS_LIST
 do
     echo $class
-    adb shell am instrument -w -e coverage true -e coverageFile $COVERAGE_FILE \
+    $ADB shell am instrument -w -e coverage true -e coverageFile $COVERAGE_FILE \
 	-e class com.mamewo.podplayer0.tests.$class \
 	-e coverage true \
 	com.mamewo.podplayer0.tests/android.test.InstrumentationTestRunner
 done
-adb pull $COVERAGE_FILE bin/coverage.ec
+$ADB pull $COVERAGE_FILE bin/coverage.ec
 java -cp $ANDROID_HOME/tools/lib/emma.jar emma report -r html -in ../../bin/coverage.em,bin/coverage.ec -sp ../../src
+mv coverage bin
 
