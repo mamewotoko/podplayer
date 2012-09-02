70e5edd5ddaabe3233f2529d8035c8975b5b0a33
Takashi Masuyama <mamewotoko@gmail.com>
2012-09-02 21:32:51 +0900

    fix order of test

--- test/podtest/run.sh
+++ test/podtest/run.sh
@@ -4,15 +4,13 @@ CLASS_LIST="PodplayerExpActivityTest PodplayerActivityTest"
 ADB=$ANDROID_HOME/platform-tools/adb
 
 #adb shell rm -f $COVERAGE_FILE
-for class in $CLASS_LIST
-do
-    echo $class
-    $ADB shell am instrument -w -e coverage true -e coverageFile $COVERAGE_FILE \
-	-e class com.mamewo.podplayer0.tests.$class \
-	-e coverage true \
-	com.mamewo.podplayer0.tests/android.test.InstrumentationTestRunner
-done
+$ADB shell am instrument -w -e coverage true -e coverageFile $COVERAGE_FILE \
+  -e class com.mamewo.podplayer0.tests.PodplayerActivityTest,com.mamewo.podplayer0.tests.PodplayerExpActivityTest,com.mamewo.podplayer0.tests.MainActivityTest \
+  -e coverage true \
+  com.mamewo.podplayer0.tests/android.test.InstrumentationTestRunner
 $ADB pull $COVERAGE_FILE bin/coverage.ec
 java -cp $ANDROID_HOME/tools/lib/emma.jar emma report -r html -in ../../bin/coverage.em,bin/coverage.ec -sp ../../src
+rm -rf bin/coverage
 mv coverage bin
 
+#  -e class com.mamewo.podplayer0.tests.PodplayerExpActivityTest,com.mamewo.podplayer0.tests.PodplayerActivityTest \
