768d35702a3bb1fa892c9e382441132d916ca786
Takashi Masuyama <mamewotoko@gmail.com>
2012-09-30 10:48:19 +0900

    download jar only if it does not exist

--- test/podtest/libs/setup.sh
+++ test/podtest/libs/setup.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 URL=http://robotium.googlecode.com/files/robotium-solo-3.2.1.jar
-wget $URL
+if [ ! -f robotium-solo-3.2.1.jar ]; then
+    wget $URL
+fi
+
 URL2=http://scirocco.googlecode.com/files/scirocco_v2.0.jar
-wget $URL2
+if [ ! -f scirocco_v2.0.jar ]; then
+    wget $URL2
+fi
