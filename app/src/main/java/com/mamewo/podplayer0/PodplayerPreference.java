6fc4acde8caf4eebb0fedcf04c8c10e50a5ee7fd
Takashi Masuyama <mamewotoko@gmail.com>
2016-10-20 12:28:01 +0900

    add action bar to preference

--- app/src/main/java/com/mamewo/podplayer0/PodplayerPreference.java
+++ app/src/main/java/com/mamewo/podplayer0/PodplayerPreference.java
@@ -3,7 +3,7 @@ package com.mamewo.podplayer0;
 import android.widget.FrameLayout;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+//import android.support.v7.widget.Toolbar;
 //dummy actionbar activity
 //http://stackoverflow.com/questions/27091845/android-appcompat-dark-theme-settings-checkbox
 public class PodplayerPreference
@@ -12,10 +12,10 @@ public class PodplayerPreference
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        FrameLayout frame = new FrameLayout(this);
-        setContentView(frame);
-        frame.setId(R.id.frame);
+        //FrameLayout frame = new FrameLayout(this);
+        //frame.setId(R.id.frame);
+        setContentView(R.layout.preference_toolbar);
         getSupportFragmentManager().beginTransaction()
-            .replace(R.id.frame, new PodplayerPreferenceFragment(), null).commit();
+            .replace(R.id.frame2, new PodplayerPreferenceFragment(), null).commit();
     }
 }
