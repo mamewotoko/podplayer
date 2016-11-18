6c4359f8189672386b9fca5a3b90b1e8f212a39b
Takashi Masuyama <mamewotoko@gmail.com>
2016-11-18 12:21:34 +0900

    modify lib version

--- ci/snapci/init_android.sh
+++ ci/snapci/init_android.sh
@@ -14,7 +14,7 @@ if [ ! -e ${INITIALIZATION_FILE} ]; then
 
     echo y | android update sdk --no-ui --filter android-19,android-10,android-24 > /dev/null
     echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
-    echo y | android update sdk --no-ui --all --filter build-tools-23.0.1 > /dev/null
+    echo y | android update sdk --no-ui --all --filter build-tools-24.0.3 > /dev/null
     echo y | android update sdk --no-ui --all --filter extra-android-support > /dev/null
     echo y | android update sdk --no-ui --filter extra-android-m2repository > /dev/null
 
