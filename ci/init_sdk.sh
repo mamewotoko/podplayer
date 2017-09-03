9ee6c38f0f902673790790921925feb6bbeeb423
Takashi Masuyama <mamewotoko@gmail.com>
2017-09-03 11:26:44 +0900

    use avdmanger/sdkmanager

--- ci/init_sdk.sh
+++ ci/init_sdk.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-echo y | android update sdk --no-ui --all --filter android-19,android-10,android-24 # > /dev/null
-echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
-echo y | android update sdk --no-ui --all --filter build-tools-24.0.3 # > /dev/null
-echo y | android update sdk --no-ui --all --filter extra-android-support > /dev/null
-echo y | android update sdk --no-ui --filter extra-android-m2repository > /dev/null
+# echo y | android update sdk --no-ui --all --filter android-19,android-10,android-24 # > /dev/null
+# echo y | android update sdk --no-ui --filter tools,platform-tools > /dev/null
+# echo y | android update sdk --no-ui --all --filter build-tools-24.0.3 # > /dev/null
+# echo y | android update sdk --no-ui --all --filter extra-android-support > /dev/null
+# echo y | android update sdk --no-ui --filter extra-android-m2repository > /dev/null
 
-echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-10 > /dev/null
-echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-19 > /dev/null
+# echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-10 > /dev/null
+# echo y | android update sdk --no-ui --all --filter sys-img-armeabi-v7a-android-19 > /dev/null
+
+echo y | sdkmanager "build-tools;26.0.1" emulator "platform-tools" "extras;android;m2repository" "extras;google;m2repository"
--- ci/snapci/02_test.sh
+++ ci/snapci/02_test.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# ANDROID_HOME=/opt/android-sdk
 #set -e
 
 ## usage
@@ -27,7 +28,8 @@ fi
 
 ABI=$5
 if [ -z "$ABI" ]; then
-    ABI=default/armeabi-v7a
+    #ABI="default;armeabi-v7a"
+    ABI="default;x86"
 fi
 
 AVD_NAME=emu_${TARGET}_${SCREEN_SIZE}_${LANGUAGE}_${COUNTRY}
@@ -36,9 +38,16 @@ AVD_NAME=emu_${TARGET}_${SCREEN_SIZE}_${LANGUAGE}_${COUNTRY}
 #android list sdk -e --all
 #echo ----
 
-echo "android create avd -n $AVD_NAME -b $ABI -t $TARGET -c 32M --skin $SCREEN_SIZE"
-echo no | android create avd --force -n $AVD_NAME --abi $ABI -t $TARGET -c 32M --skin $SCREEN_SIZE || exit 1
-emulator -avd $AVD_NAME -prop persist.sys.language=$LANGUAGE -prop persist.sys.country=$COUNTRY -no-window &
+echo y | sdkmanager "build-tools;26.0.1" "system-images;${TARGET};${ABI}" emulator "platform-tools" "platforms;${TARGET}" "extras;android;m2repository" "extras;google;m2repository"
+
+#echo "android create avd -n $AVD_NAME -b $ABI -c 32M -k "
+#echo no | android create avd --force -n $AVD_NAME -c 32M  || exit 1
+echo "avdmanager create avd -n $AVD_NAME -c 32M -f -k system-images;${TARGET};${ABI}"
+
+echo no | avdmanager create avd -n $AVD_NAME -c 32M -f -k "system-images;${TARGET};${ABI}" || exit 1
+
+#( emulator -avd $AVD_NAME -prop persist.sys.language=$LANGUAGE -prop persist.sys.country=$COUNTRY -no-window ) &
+( cd $ANDROID_HOME/tools; emulator -avd $AVD_NAME -prop persist.sys.language=$LANGUAGE -prop persist.sys.country=$COUNTRY -gpu off -no-window ) &
 EMULATOR_PID=$!
 
 sleep 90
@@ -55,22 +64,23 @@ echo STATUS: stopped is expected: $STATUS
 #     exit 1
 # fi
 #adb uninstall com.mamewo.podplayer0 || true
-{
-    ./gradlew uninstallAll installDebug
-    adb logcat -v time > app/build/logcat.log &
 
-    ./gradlew spoonDebug -PspoonOutput=spoon_${AVD_NAME}
-    ./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerExpActivity -PspoonOutput=spoon_exp_${AVD_NAME}
-    ./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerCardActivity -PspoonOutput=spoon_card_${AVD_NAME}
-    ./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodcastListPreference -PspoonOutput=spoon_podlist_${AVD_NAME}
-    ./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerActivityLand -PspoonOutput=spoon_land_${AVD_NAME}
-}
+./gradlew uninstallAll installDebug
+adb logcat -v time > app/build/logcat.log &
+
+./gradlew spoonDebug -PspoonOutput=spoon_${AVD_NAME}
+./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerExpActivity -PspoonOutput=spoon_exp_${AVD_NAME}
+./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerCardActivity -PspoonOutput=spoon_card_${AVD_NAME}
+./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodcastListPreference -PspoonOutput=spoon_podlist_${AVD_NAME}
+./gradlew spoonDebug -PspoonClassName=com.mamewo.podplayer0.tests.TestPodplayerActivityLand -PspoonOutput=spoon_land_${AVD_NAME}
+
 
 # finally
 ## TODO: get serial id
 SERIALNO=$(adb get-serialno)
-adb -s emulator-$SERIALNO emu kill
+adb -s $SERIALNO emu kill
 sleep 5
 kill -9 $EMULATOR_PID || true
-android delete avd -n $AVD_NAME
+avdmanager delete avd -n $AVD_NAME
 
+# TODO: uninstall package sdk
