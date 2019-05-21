e1df6e785d1998f3d78b5a86ad323c4187c2fa1f
Takashi Masuyama <mamewotoko@gmail.com>
2019-05-22 08:08:10 +0900

    debug travis

--- ci/upload.sh
+++ ci/upload.sh
@@ -4,7 +4,5 @@
 COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
 #./gdrive delete -r app-release.apk reports || true
-ARCHIVE=app/build/outputs/apk/release/app-release.apk
-./gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR --name ap-release-${COMMIT}.apk "$ARCHIVE"
-ARCHIVE=./app/build/reports/
-./gdrive upload -r --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR --name reports-${COMMIT} "$ARCHIVE"
+ARCHIVE=app/build
+./gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
