3cb826d72f390c7a16b515ce96d197f0bf643b75
Takashi Masuyama <mamewotoko@gmail.com>
2019-05-22 12:16:58 +0900

    upload build dir

--- ci/upload.sh
+++ ci/upload.sh
@@ -3,6 +3,4 @@
 
 COMMIT=$(echo $TRAVIS_COMMIT | sed 's!^\(.\{10\}\).*!\1!')
 
-#./gdrive delete -r app-release.apk reports || true
-ARCHIVE=app/build
-./gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
+./gdrive upload --recursive --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR app/build
