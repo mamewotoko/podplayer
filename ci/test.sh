065ccf4d138c9d590054a8759048a12db73dbbfd
Takashi Masuyama <mamewotoko@gmail.com>
2019-07-15 12:07:20 +0900

    install libc6 to run test

--- ci/test.sh
+++ ci/test.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
+set -ex
+
 bash -x ci/snapci/02_test.sh en us 480x800 android-19 "default;x86"
+curl -L 'https://drive.google.com/uc?id=1Ej8VgsW5RgK66Btb9p74tSdHMH3p4UNb&export=download' > gdrive
+chmod +x gdrive
 sh ci/upload.sh app/build/
