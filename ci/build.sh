2cec12903cf9a087d14e47683f6718860b7c0034
Takashi Masuyama <mamewotoko@gmail.com>
2019-07-24 21:31:10 +0900

    add gdrive binary

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
 ./gradlew build
-curl -L 'https://drive.google.com/uc?id=1Ej8VgsW5RgK66Btb9p74tSdHMH3p4UNb&export=download' > gdrive
-chmod +x gdrive
 sh ci/upload.sh app/build/outputs/apk
